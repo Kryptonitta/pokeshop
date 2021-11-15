f71a32e69d1bb94139a4188a76a647aa356e0203
kryptonitta <karina.zwenger@live.com.ar>
2021-11-15 15:57:28 -0300

    ars

--- src/components/itemDetail/index.jsx
+++ src/components/itemDetail/index.jsx
@@ -11,7 +11,7 @@ const ItemDetail = ({ name, description, stock, photo, price, onAdd }) => {
                 </div>
                 <div>
                     <h3>{name}</h3>
-                    <p> {Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(price)}</p>
+                    <p> {Intl.NumberFormat('es-CL', { style: 'currency', currency: 'ARS' }).format(price)}</p>
                     <p>{description}</p>
                     {/* <ItemCount stock={stock} initial={0} onAdd={onAdd} name={name} /> */}
                 </div>
